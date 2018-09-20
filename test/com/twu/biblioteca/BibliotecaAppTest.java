2049fc3f6e949ec7babc1c060642ba184953f344
John Yong <johnyong@thoughtworks.com>
2018-09-20 14:22:47 +0800

    Test welcome message is output to stdout

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,13 +1,33 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertTrue;
 
 public class BibliotecaAppTest {
 
+    private ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+    private PrintStream stdoutStream = System.out;
+
+    @Before
+    public void setUpStream() {
+        System.setOut(new PrintStream(outStream));
+    }
+
+    @After
+    public void restoreStream() {
+        System.setOut(stdoutStream);
+    }
+
     @Test
     public void welcomeTest() {
-        assertEquals("Welcome!", BibliotecaApp.welcome());
+        BibliotecaApp.main(new String[] {});
+        assertTrue(outStream.toString().startsWith("Welcome!"));
     }
 }
