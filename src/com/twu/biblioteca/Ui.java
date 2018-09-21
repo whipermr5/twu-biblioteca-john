83d0136f02b0d5ddd2e1cdde7b5560aa5c92b633
John Yong <johnyong@thoughtworks.com>
2018-09-21 12:24:02 +0800

    Extract book details string constants into Ui

--- src/com/twu/biblioteca/Ui.java
+++ src/com/twu/biblioteca/Ui.java
@@ -9,13 +9,17 @@ class Ui {
     static final String ID_LIST_BOOKS = "l";
     static final String ID_QUIT = "q";
 
-    private static final String BOOK_DETAILS_FORMAT_STRING = "%n%-40s | %-20s | %4s";
+    static final String BOOK_DETAILS_FORMAT_STRING = "%n%-40s | %-20s | %4s";
+    static final String BOOK_LIST_HEADER = "Books:"
+            + String.format(Ui.BOOK_DETAILS_FORMAT_STRING, "Title", "Author", "Year") + System.lineSeparator()
+            + "----------------------------------------------------------------------";
 
     static final String WELCOME = "Welcome!";
     static final String MENU = "Please select an option:" + System.lineSeparator()
             + ID_LIST_BOOKS + " - List Books" + System.lineSeparator()
             + ID_QUIT + " - Quit";
     static final String INVALID_OPTION = "Select a valid option!";
+    static final String NO_BOOKS = "No books!";
     static final String GOODBYE = "Goodbye!";
 
     static String getUserInput(InputStream in) {
@@ -29,17 +33,13 @@ class Ui {
         }
 
         if (books.isEmpty()) {
-            return "No books!";
+            return NO_BOOKS;
         }
 
-        StringBuilder sb = new StringBuilder("Books:");
-        sb.append(String.format(BOOK_DETAILS_FORMAT_STRING, "Title", "Author", "Year"));
-        sb.append("\n----------------------------------------------------------------------");
-
+        StringBuilder sb = new StringBuilder(BOOK_LIST_HEADER);
         for (Book book : books) {
             sb.append(String.format(BOOK_DETAILS_FORMAT_STRING, book.getTitle(), book.getAuthor(), book.getYear()));
         }
-
         return sb.toString();
     }
 }
--- test/com/twu/biblioteca/UiTest.java
+++ test/com/twu/biblioteca/UiTest.java
@@ -11,14 +11,6 @@ import static org.junit.Assert.assertNull;
 
 public class UiTest {
 
-    private static final String NO_BOOKS = "No books!";
-
-    private static final String BOOK_DETAILS_FORMAT_STRING = "%n%-40s | %-20s | %4s";
-
-    private static final String BOOK_LIST_HEADER = "Books:"
-            + String.format(BOOK_DETAILS_FORMAT_STRING, "Title", "Author", "Year")
-            + "\n----------------------------------------------------------------------";
-
     private static final String BOOK1_TITLE = "TDD";
     private static final String BOOK1_AUTHOR = "Kent";
     private static final int BOOK1_YEAR = 2002;
@@ -39,15 +31,15 @@ public class UiTest {
     public void testFormatBookList() {
         assertNull(Ui.formatBookList(null));
 
-        assertEquals(NO_BOOKS, Ui.formatBookList(Collections.emptyList()));
+        assertEquals(Ui.NO_BOOKS, Ui.formatBookList(Collections.emptyList()));
 
-        assertEquals(BOOK_LIST_HEADER
-                        + String.format(BOOK_DETAILS_FORMAT_STRING, BOOK1_TITLE, BOOK1_AUTHOR, BOOK1_YEAR),
+        assertEquals(Ui.BOOK_LIST_HEADER
+                        + String.format(Ui.BOOK_DETAILS_FORMAT_STRING, BOOK1_TITLE, BOOK1_AUTHOR, BOOK1_YEAR),
                 Ui.formatBookList(Collections.singletonList(new Book(BOOK1_TITLE, BOOK1_AUTHOR, BOOK1_YEAR))));
 
-        assertEquals(BOOK_LIST_HEADER
-                        + String.format(BOOK_DETAILS_FORMAT_STRING, BOOK1_TITLE, BOOK1_AUTHOR, BOOK1_YEAR)
-                        + String.format(BOOK_DETAILS_FORMAT_STRING, BOOK2_TITLE, BOOK2_AUTHOR, BOOK2_YEAR),
+        assertEquals(Ui.BOOK_LIST_HEADER
+                        + String.format(Ui.BOOK_DETAILS_FORMAT_STRING, BOOK1_TITLE, BOOK1_AUTHOR, BOOK1_YEAR)
+                        + String.format(Ui.BOOK_DETAILS_FORMAT_STRING, BOOK2_TITLE, BOOK2_AUTHOR, BOOK2_YEAR),
                 Ui.formatBookList(Arrays.asList(
                         new Book(BOOK1_TITLE, BOOK1_AUTHOR, BOOK1_YEAR),
                         new Book(BOOK2_TITLE, BOOK2_AUTHOR, BOOK2_YEAR))));
