bf48eb7c3b968bad3cc109600ec10d446c0bab66
John Yong <johnyong@thoughtworks.com>
2018-09-26 01:37:41 +0800

    Protect checkout with login; implement proper bookkeeping

--- src/com/twu/biblioteca/command/CheckoutBookCommand.java
+++ src/com/twu/biblioteca/command/CheckoutBookCommand.java
@@ -1,7 +1,8 @@
 package com.twu.biblioteca.command;
 
-import com.twu.biblioteca.library.Library;
+import com.twu.biblioteca.common.Session;
 import com.twu.biblioteca.common.Ui;
+import com.twu.biblioteca.library.Library;
 
 import java.io.InputStream;
 import java.io.PrintStream;
@@ -13,13 +14,18 @@ public class CheckoutBookCommand extends BookCommand {
             out.println(Ui.NO_BOOKS_AVAILABLE);
             return;
         }
+        new LoginCommand().execute(library, in, out);
+        Session session = library.getSession();
+        if (!session.isUserLoggedIn()) {
+            return;
+        }
         out.println(Ui.formatBooksAvailable(getAvailableBooks(library)));
         String bookId = Ui.getUserInput(in, out, Ui.SELECT_BOOK_CHECKOUT);
         if (!isBook(library.getItem(bookId))) {
             out.println(Ui.CHECKOUT_BOOK_FAILURE);
             return;
         }
-        boolean wasSuccessful = library.checkoutItem(bookId, "user");
+        boolean wasSuccessful = library.checkoutItem(bookId, session.getCurrentUser().getUsername());
         out.println(wasSuccessful ? Ui.CHECKOUT_BOOK_SUCCESS : Ui.CHECKOUT_BOOK_FAILURE);
     }
 }
--- test/com/twu/biblioteca/command/CheckoutBookCommandTest.java
+++ test/com/twu/biblioteca/command/CheckoutBookCommandTest.java
@@ -13,6 +13,7 @@ import java.io.OutputStream;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class CheckoutBookCommandTest {
 
@@ -21,15 +22,26 @@ public class CheckoutBookCommandTest {
         Library library = new Library();
         Command command = CommandFactory.get(Ui.ID_CHECKOUT_BOOK);
 
-        Book book = BookCommand.getAvailableBooks(library).iterator().next();
-        InputStream in = new ByteArrayInputStream(book.getId().getBytes());
+        String credentials = "invalid username" + System.lineSeparator() + "some password";
+        InputStream in = new ByteArrayInputStream(credentials.getBytes());
         OutputStream out = new ByteArrayOutputStream();
-        String expected = Ui.formatBooksAvailable(BookCommand.getAvailableBooks(library)) + System.lineSeparator()
+        String expected = Ui.LOGIN_PROMPT_USERNAME + Ui.LOGIN_PROMPT_PASSWORD + System.lineSeparator()
+                + Ui.LOGIN_FAILURE + System.lineSeparator();
+        command.execute(library, in, out);
+        assertEquals(expected, out.toString());
+
+        assertTrue(library.getSession().login("valid username", "valid password"));
+
+        Book book = BookCommand.getAvailableBooks(library).iterator().next();
+        in = new ByteArrayInputStream(book.getId().getBytes());
+        out = new ByteArrayOutputStream();
+        expected = Ui.formatBooksAvailable(BookCommand.getAvailableBooks(library)) + System.lineSeparator()
                 + Ui.SELECT_BOOK_CHECKOUT + System.lineSeparator()
                 + Ui.CHECKOUT_BOOK_SUCCESS + System.lineSeparator();
         command.execute(library, in, out);
         assertEquals(expected, out.toString());
         assertFalse(BookCommand.getAvailableBooks(library).contains(book));
+        assertTrue(BookCommand.getBooksBorrowedBy(library, "valid username").contains(book));
 
         in = new ByteArrayInputStream(book.getId().getBytes());
         out = new ByteArrayOutputStream();
